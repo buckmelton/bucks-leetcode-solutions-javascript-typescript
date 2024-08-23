c9c4151c1aef96718f44315f051319bb956c0fd5
Buck Melton <buckm@alumni.stanford.edu>
2024-08-23 11:50:23 -0700

    Solve LettCode 283: Move Zeroes

--- moveZeroes.ts
+++ moveZeroes.ts
@@ -1,5 +1,27 @@
 /**
- Notes:
+
+LeetCode 283: Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+
+
+ My Notes:
 
  This is defined as a two-pointer problem.
  
@@ -39,5 +61,41 @@
 
   */
 function moveZeroes(nums: number[]): void {
+  let zeroSearchPtr: number = 0;
+  let nonZeroSearchPtr: number;
+  let tmp: number;
+  // Find first zero
+  while ((nums[zeroSearchPtr] != 0) && (zeroSearchPtr < nums.length)) {
+    zeroSearchPtr++;
+  }
+  // If no zeroes in array, we're done
+  if (zeroSearchPtr >= nums.length) {
+    return;
+  }
+  // We've found first zero, first candidate non zero is the next element
+  nonZeroSearchPtr = zeroSearchPtr + 1;
+  // guaranteed to exit because nzsp is incremented every pass and will eventually
+  // trigger RETURN
+  while (true) {
+    // find next non zero
+    while ((nums[nonZeroSearchPtr] == 0) && (nonZeroSearchPtr < nums.length)) {
+      nonZeroSearchPtr++;
+    }
+    // if no more non zeroes, we're done
+    if (nonZeroSearchPtr >= nums.length) {
+      return;
+    }
+    // Assert: zsp points to leftmost zero, nzsp points to next non zero
+    // Swap them, the leftmost zero is now pushed forward by one
+    tmp = nums[zeroSearchPtr];
+    nums[zeroSearchPtr] = nums[nonZeroSearchPtr];
+    nums[nonZeroSearchPtr] = tmp;
+    zeroSearchPtr++;
+  }
+};
 
+function myLog(location: string, zsp: number, nzsp: number, nums: number[]) {
+  console.log(`***********`);
+  console.log(`location: ${location}`);
+  console.log(`zsp: ${zsp}, nzsp: ${nzsp}, nums: ${nums}`);
 };
