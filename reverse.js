188f123f8ecb58bb676ad1b60ced0f4eee0c72b7
Buck Melton <buckm@alumni.stanford.edu>
2024-12-12 11:58:05 -0800

    Add pseudocode

--- reverse.js
+++ reverse.js
@@ -1,3 +1,4 @@
+/*
 Leetcode 7. Reverse Integer
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
@@ -17,3 +18,14 @@ Output: 21
  
 Constraints:
 -231 <= x <= 231 - 1
+
+PSEUDOCODE:
+Convert integer to string
+Strip and store negative
+Strip trailing zeros
+Reverse string
+If string is less than largest 32-bit negative integer or greater
+than largest 32-bit positive integer, return 0
+Return string
+
+*/
