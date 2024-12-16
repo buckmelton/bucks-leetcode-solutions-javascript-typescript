dddbdbaf217be59932da0c73105f4e1ad5c46255
Buck Melton <buckm@alumni.stanford.edu>
2024-12-16 13:07:54 -0800

    Add pseudocode for Leetcode 21 'Merge Two Sorted Lists'

--- mergeTwoLists.ts
+++ mergeTwoLists.ts
@@ -29,3 +29,35 @@ The number of nodes in both lists is in the range [0, 50].
 Both list1 and list2 are sorted in non-decreasing order.
 
 */
+
+/*
+ Another straightforward common problem, using simple traversal of two linked lists.
+
+ PSEUDOCODE:
+ We are given the heads of two linked lists, list1 and list2
+
+ - Initialize mergedList head to the empty list
+ - Initialize mergedList tail to mergedList head
+ - Initialize current list 1 pointer to list1 head
+ - Initialize current list 2 point to list2 head
+
+ - While neither list head is null
+  - If cur list1 ptr value <= cur list2 ptr value
+    - merged list tail.next = cur list 1 ptr
+    - merged list tail = merged list tail.next
+    - cur list 1 ptr = cur list 1 ptr.next
+    - merged list tail.next = null
+  - Else (cur list1 ptr value) > cur list 1 ptr
+    - merged list tail.next = cur list 2 ptr
+    - merged list tail = merged list tail.next
+    - cur list 2 ptr = cur list 2 ptr.next
+    - merged list tail.next = null
+
+- If cur list1 pointer is null
+  - mergedList tail.next = cur list 2 pointer
+- Else (cur list2 pointer is null)
+  - mergedList tail.net = cur list 1 pointer
+
+- Return mergedList head
+
+*/ 
