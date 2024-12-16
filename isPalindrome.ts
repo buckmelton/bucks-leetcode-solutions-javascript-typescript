c213137db0ea0fe6b6f3ea527332cb033bc2e92c
Buck Melton <buckm@alumni.stanford.edu>
2024-12-16 12:06:18 -0800

    Add pseudocode for Leetcode 9 'Palindrome Number'

--- isPalindrome.ts
+++ isPalindrome.ts
@@ -25,3 +25,30 @@ Constraints:
 
 -2**31 <= x <= 2**31 - 1
 */
+
+/*
+PSEUDOCODE:
+
+This is pretty straightforward and palindrome detection is a
+well-trodden problem.
+
+Two-pointer solution.  The solution really has nothing to
+do with the numeric aspects of the input, it has only to
+do with the characters that make up the input, and so we'll
+treat this as an array of characters, or a string.  Since the problem
+statement explicitly includes the negative sign as part of the input to
+be considered as a palindrome, we don't need to special case it (e.g.
+discard it). In fact, if we knew that many of the inputs were negative
+numbers, we could save the tiniest bit of space by inspecting the first
+character and returning false if it's the '-' sign (since the last
+character will never be a '-'), but this probably isn't worth it.
+
+- Convert number to string
+- Initialize left pointer to first character
+- Initialized right pointer to character
+- While right pointer is to the right of the left pointer
+  - If left char doesn't equal right char, return false
+  - Increment left pointer and decrement right pointer
+- If we've exited the loop without already returning, then
+  the number is a palindrome.
+*/
