b80f6e5962dc8d51773046e841df008fbb6ae5b1
Buck Melton <buckm@alumni.stanford.edu>
2024-12-17 10:05:56 -0800

    Implement correct solution for Leetcode 21 'Merge Two Sorted Lists'

--- mergeTwoLists.ts
+++ mergeTwoLists.ts
@@ -61,3 +61,63 @@ Both list1 and list2 are sorted in non-decreasing order.
 - Return mergedList head
 
 */ 
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  let mergedListHead: ListNode = null;
+  let mergedListTail: ListNode = mergedListHead;
+  let curList1Ptr: ListNode = list1;
+  let curList2Ptr: ListNode = list2;
+
+  // If either list is null, we can just return the non-null list
+  if (list1 === null) {
+    return list2;
+  }
+  if (list2 === null) {
+    return list1;
+  }
+
+  // ASSERT: both list1 and list2 have a least one node with a value
+
+  // Find first node so we can initialize mergedListHead to an actual node
+  if (list1.val < list2.val ) {
+    mergedListHead = list1;
+    mergedListTail = list1;
+    curList1Ptr = list1.next;
+  } else {
+    mergedListHead = list2;
+    mergedListTail = list2;
+    curList2Ptr = list2.next;
+  }
+  // We've connected the first node to the merged list, and advanced the pointer
+  // for the involved list to the next node, we can now sever the merged node from
+  // its list
+  mergedListTail.next = null;
+
+  while ((curList1Ptr !== null) && (curList2Ptr !== null)) {
+    if (curList1Ptr.val < curList2Ptr.val) {
+      // Add list1 node to merged list and advance cur list1 pointer
+      mergedListTail.next = curList1Ptr;
+      curList1Ptr = curList1Ptr.next;
+    } else {
+      // Add list2 node to merged list and advance cur list2 pointer
+      mergedListTail.next = curList2Ptr;
+      curList2Ptr = curList2Ptr.next;
+    }
+    // Advance merged list tail
+    mergedListTail = mergedListTail.next;
+    // Sever list node
+    mergedListTail.next = null;
+  }
+
+  // ASSERT: at least one of list1 or list2 is now null
+
+  if (curList1Ptr === null) {
+    // Add rest of list2 to merged list
+    mergedListTail.next = curList2Ptr;
+  } else { // curList2Ptr === null
+    // Add rest of list1 to merged list
+    mergedListTail.next = curList1Ptr;
+  }
+  // We have our merged list, we don't need to care about any other pointer manipulation
+  return mergedListHead;
+};
