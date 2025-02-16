2e2dac9f1c886186859429488e10a64da974e061
Buck Melton <buckm@alumni.stanford.edu>
2025-02-15 20:34:46 -0800

    Add given supporting code

--- maxDepth.ts
+++ maxDepth.ts
@@ -18,3 +18,21 @@ Constraints:
 The number of nodes in the tree is in the range [0, 10**4].
 -100 <= Node.val <= 100
 */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    
+};
