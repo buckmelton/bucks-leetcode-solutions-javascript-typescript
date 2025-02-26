1c84677406026c746d400a71e39dbc4d47ee4a1b
Buck Melton <buckm@alumni.stanford.edu>
2025-02-25 23:31:53 -0800

    Add supporting code for Tree definition

--- searchBST.ts
+++ searchBST.ts
@@ -1,6 +1,6 @@
 /*
 
-700. Search in a Binary Search Tree
+Leetcode 700: Search in a Binary Search Tree
 You are given the root of a binary search tree (BST) and an integer val.
 
 Find the node in the BST that the node's value equals val and return the subtree
@@ -22,3 +22,21 @@ root is a binary search tree.
 1 <= val <= 10**7
 
 */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+
+};
