66282c72d570df870ac9cb8a18f824225df9f6f3
Buck Melton <buckm@alumni.stanford.edu>
2025-03-05 09:36:03 -0800

    Add description of Two Sum problem

---
+++ 2sum.ts
@@ -0,0 +1,29 @@
+/*
+Two Sum problem
+Return all pairs of unique indices whose values add up to a target sum.
+
+Example 1:
+Input: [1, 5, 7, -1, 5], target= 6 
+Output: [[1, 5], [-1, 7]]
+Explanation: There are only two pairs that add up to 6: [1, 5] and [-1, 7].
+
+Example 2:
+Input: [1, 9, -1, 8, 6], target = 4
+Output: [[]]
+Explanation: No pairs add up to 4.
+
+Example 3:
+Input: [3, 5, 2, -4, 8, 11], target = 7
+Output: [[11, -4], [2, 5]]
+Explanation: 11 + -4 = 7 and 2 + 5 = 7
+
+Example 4:
+Input: [2,6,5,8,11], target = 14
+Outpu: [1, 3] 
+Explanation: arr[1] + arr[3] = 14
+
+Example 5:
+Input: [2,6,5,8,11], target = 15
+Result: [-1, -1]
+Explanation: There exist no such two numbers whose sum is equal to the target.
+*/
