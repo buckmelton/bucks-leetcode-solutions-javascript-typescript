bd98df4570abe83c337a7b2f2ba04bba2eceabf6
Buck Melton <buckm@alumni.stanford.edu>
2025-03-17 12:47:13 -0700

    Add my pre-coding notes & observations & analysis

--- leetCode199rightSideView.ts
+++ leetCode199rightSideView.ts
@@ -24,3 +24,19 @@ Constraints:
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
+
+/*
+ BUCK'S NOTES:
+ We've been given the advice that this is a BFS (Breadth First Search) problem.
+ I observe that the values that we want as part of the output, are in order
+ with regards to a BFS, but they aren't every value in a tree.  More specifically,
+ the values we want are the last value of each level.
+ BFS is usually implemented as a queue, where all the nodes of a particular level
+ end up contiguous in the queue, and (though this is not a requirement of BFS)
+ typically they are put in the queue from left to right.
+
+ So all we need to know, is where the nodes of one level end, and the next level
+ begins.  Which means we need to track the levels of the nodes, or at least track
+ when there is an increase in the level. When there is an increase in the level,
+ we can push the value onto the array of results.
+ */
