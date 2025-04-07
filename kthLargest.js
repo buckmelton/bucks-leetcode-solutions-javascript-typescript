09000d0ff7679f76a71b6955f240d192670e0fa9
Buck Melton <buckm@alumni.stanford.edu>
2025-04-07 12:08:49 -0700

    Add correct implementation

--- kthLargest.js
+++ kthLargest.js
@@ -47,3 +47,66 @@ PSEUDOCODE FOR EXTRACT:
   - swap
 */
 
+function swap(arr, i, j) {
+  let temp = undefined;
+  temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function minHeapInsert(minHeap, num) {
+  minHeap.push(num);
+  let curPos = minHeap.length - 1;
+
+  while ( ( curPos > 0 ) && ( minHeap[curPos] < minHeap[Math.floor((curPos-1)/2)] ) ) {
+    swap(minHeap, curPos, Math.floor((curPos-1)/2));
+    curPos = Math.floor((curPos-1)/2);
+  }
+}
+
+function bubbleDown(minHeap) {
+  let curPos = 0;
+  let leftChildPos = curPos * 2 + 1;
+  let rightChildPos = curPos * 2 + 2;
+  let leftChildExists = leftChildPos < minHeap.length;
+  let rightChildExists = rightChildPos < minHeap.length;
+  let finished = false;
+  while (!finished) {
+    if ( leftChildExists && rightChildExists &&
+      ( ( ( minHeap[curPos] > minHeap[leftChildPos] ) || ( minHeap[curPos] > minHeap[rightChildPos] ) ) ) ) {
+      let swapPos = minHeap[leftChildPos] < minHeap[rightChildPos] ? leftChildPos : rightChildPos;
+      swap( minHeap, curPos, swapPos );
+      curPos = swapPos;
+    } else {
+      if ( leftChildExists && ( minHeap[curPos] > minHeap[leftChildPos] ) ) {
+        swap( minHeap, curPos, leftChildPos );
+        curPos = leftChildPos;
+      } else {
+        finished = true;
+      }
+    }
+    leftChildPos = curPos * 2 + 1;
+    rightChildPos = curPos * 2 + 2;
+    leftChildExists = leftChildPos < minHeap.length;
+    rightChildExists = rightChildPos < minHeap.length;
+  }
+} 
+
+function minHeapExtract(minHeap) {
+  let minVal = minHeap[0];
+  minHeap[0] = minHeap.pop();
+  bubbleDown(minHeap);
+  return minVal;
+}
+
+var findKthLargest = function(nums, k) {
+  let minHeap = [];
+  for (const num of nums) {
+    minHeapInsert(minHeap, num);
+    if (minHeap.length > k) {
+      let minVal = minHeapExtract(minHeap);
+    }
+  }
+
+  return minHeap[0];    
+};
