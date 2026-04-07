b7b4dd1ec483dee98a21775a3360f0d5ec43cb3b
Buck <buckm@alumni.stanford.edu>
2026-04-07 07:57:25 -0700

    Add mergeAlternately function for string merging
    Implement function to merge two strings alternately.

---
+++ mergeAlternately.js
@@ -0,0 +1,20 @@
+// Leetcode 1768. Merge Strings Alternately
+// You are given two strings word1 and word2.
+// Merge the strings by adding letters in alternating order, starting with word1.
+// If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+var mergeAlternately = function(word1, word2) {
+  result = "";
+  let maxLen = Math.max(word1.length, word2.length);
+  for (let i = 0; i < maxLen; i++) {
+    if (i < word1.length) {
+      result += word1[i];
+    }
+    if (i < word2.length) {
+      result += word2[i];
+    }
+  }
+  return result;
+};
