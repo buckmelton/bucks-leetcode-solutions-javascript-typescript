131a73653736dbd02fe531fc8dbefa0c5c264f3f
Buck <buckm@alumni.stanford.edu>
2026-04-08 10:41:54 -0700

    Implement findMaxAverage function for maximum average calculation

--- findMaxAverage.js
+++ findMaxAverage.js
@@ -15,3 +15,34 @@
 // n == nums.length
 // 1 <= k <= n <= 10^5
 // -10^4 <= nums[i] <= 10^4
+
+// Pseudocode:
+// (We are assured there are at least k elements in the arr)
+// Init cur sum to sum of first k elements
+// Init max sum to cur sum
+// Init window start index to 1
+// While window start index <= arr length - k:
+//   Subtract window start index - 1 from cur sum
+//   Add window start index + k - 1 to cur sum
+//   If cur sum > max sum:
+//     Replace max sum with cur sum
+//   Increment window start index
+// Return max sum / k
+
+var findMaxAverage = function(nums, k) {
+  let curSum = 0;
+  for (let i = 0; i < k; i++) {
+    curSum += nums[i];
+  }
+  let maxSum = curSum;
+  let winStart = 1;
+  while (winStart <= nums.length - k) {
+    curSum -= nums[winStart - 1];
+    curSum += nums[winStart + k - 1];
+    if (curSum > maxSum) {
+      maxSum = curSum;
+    }
+    winStart++;
+  }
+  return maxSum / k;
+};
